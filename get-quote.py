2dba1d3afcd30608a145d91b08168382fbd5df98
semicon.tester <semicon.tester@gmail.com>
2021-10-29 07:37:25 -0300

    update from pte

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,2 @@
-import random
-
-def go():
-    print("Keep it logically awesome.")
-
-    f = open("quotes.txt")
-    quotes = f.readlines()
-    f.close()
-
-    last = 13
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
-
-if __name__== "__main__":
-  go()
-# comment
-# comment 2
+# new lines
+#thats all
